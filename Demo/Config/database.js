91363877979ea41ce3077b8f64d3d7acfe1ecb92
Mohanad Fawzy <mohanad.salem159@gmail.com>
2023-09-16 06:22:47 +0300

    idk wtf am i doing

--- Demo/Config/database.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    database: 'mongodb://0.0.0.0:27017',
-    secret: 'password'
-}
--- Demo/index.js
+++ Demo/index.js
@@ -6,39 +6,8 @@ const bodyParser = require('body-parser')
 const cors = require('cors')
 const passport = require('passport')
 const mongoose = require('mongoose')
-const config = require('./Config/database')
 
 //!----------------------
 
 //? Start server --------
 
-mongoose.connect(config.database)
-
-mongoose.connection.on('connected',()=>{
-    console.log('connected to database ' + config.database)
-} )
-
-mongoose.connection.on('error',(err)=>{
-    console.log('ERROR IS ' + err)
-} )
-
-const app = express()
-const port = 3000
-
-const users = require('./routs/users')
-
-//? Middleware
-
-app.use(cors()) 
-app.use(bodyParser.json())
-app.use('/users' , users)
-app.use(express.static(path.join('/Frontend')))
-
-
-app.get('/',(req,res)=>{
-    res.send('Invalid')
-})
-
-app.listen(port ,() =>{
-    console.log('server is on port '+ port)
-})
