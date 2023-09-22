eff9b121b67b4afda80981e12f6e578c6e0c6412
Mohanad Fawzy <mohanad.salem159@gmail.com>
2023-09-22 04:13:46 +0300

    🎨 OCD forced me to double check my progress

--- Backend/modules/providers.js
+++ Backend/modules/providers.js
@@ -3,6 +3,7 @@ var providers = [
     id: "001",
     name: "Ahmed",
     position: "Janitor",
+    salary: "-150",
     company: {
       name: "Google",
       address: "Asafra",
@@ -12,6 +13,7 @@ var providers = [
     id: "002",
     name: "Mohammed",
     position: "Lawyer",
+    salary: "5800",
     company: {
       name: "Microsoft",
       address: "Asafra",
@@ -19,8 +21,9 @@ var providers = [
   },
   {
     id: "003",
-    name: "Aly",
+    name: "Marawan",
     position: "aaa",
+    salary: "0.5",
     company: {
       name: "IBM",
       address: "Asafra",
@@ -30,9 +33,30 @@ var providers = [
     id: "004",
     name: "Mohanad",
     position: "CEO",
+    salary: "1*10^15",
     company: {
       name: "Beesoft",
       address: "SediBeshr",
+    },
+  },
+  {
+    id: "005",
+    name: "Gad",
+    position: "Btenganah",
+    salary: "2500",
+    company: {
+      name: "Al tawheed we el nour",
+      address: "Not found",
+    },
+  },
+  {
+    id: "006",
+    name: "Abou Mohammed",
+    position: "Trader",
+    salary: "20000",
+    company: {
+      name: "The Wekala",
+      address: "Sand station",
     },
   },
 ];
